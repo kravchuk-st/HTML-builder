4e24e074438a43789bb93586d18dc45a798dc085
kravchuk-st <qkp@ukr.net>
2022-05-22 21:35:07 +0300

    refactor: change message

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,7 +4,7 @@ const outPut = path.join(__dirname, 'text.txt');
 const { stdin, stdout, exit } = process;
 const stream = fs.createWriteStream(outPut);
 
-stdout.write('Write a some text:\n');
+stdout.write('Write some text:\n');
 stdin.on('data', (data) => {
   let str = data.toString();
   if (str.trim().toLowerCase() === 'exit') {
